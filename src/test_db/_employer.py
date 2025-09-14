f4a1ead5f9b2a98ab5e61ba22f90a7fb6c36d1e0
Christopher Kauffman <chriskauffman@vollgas.net>
2025-09-13 22:10:25 -0500

    feat: Employer needed MultipleJoins to Job and alternate_id is now an alternateId

--- src/test_db/_employer.py
+++ src/test_db/_employer.py
@@ -1,7 +1,8 @@
 import logging
 
 import faker
-from sqlobject import StringCol  # type: ignore
+import nanoid
+from sqlobject import MultipleJoin, SQLMultipleJoin, StringCol  # type: ignore
 
 from ._test_db_sqlobject import TestDBSQLObject
 
@@ -16,9 +17,14 @@ class Employer(TestDBSQLObject):
     Attributes:
         name (StringCol): the name of the settings
         alternate_id (StringCol): an alternate ID for the employer
+        jobs (MultipleJoin): the jobs for the employer
+        jobs_select (SQLMultipleJoin): the jobs for the employer
     """
 
     _gid_prefix: str = "e"
 
     name: StringCol = StringCol(alternateID=True, default=fake.company)
-    alternate_id: StringCol = StringCol(default=None)
+    alternate_id: StringCol = StringCol(alternateID=True, default=nanoid.generate)
+
+    jobs: MultipleJoin = MultipleJoin("Job")
+    jobs_select: SQLMultipleJoin = SQLMultipleJoin("Job")
