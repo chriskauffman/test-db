9691883fb63018c022a1fc3be494c1917ddb9430
Christopher Kauffman <chriskauffman@vollgas.net>
2025-10-12 17:13:53 -0500

    feat: working subcommands

--- src/test_db/main.py
+++ src/test_db/main.py
@@ -11,12 +11,8 @@ import pathlib
 import shutil
 import sys
 
-from pydantic import Field, SecretStr, ValidationError
-from pydantic_settings import (
-    BaseSettings,
-    CliSuppress,
-    SettingsConfigDict,
-)
+from pydantic import BaseModel, Field, SecretStr, ValidationError
+from pydantic_settings import BaseSettings, CliApp, CliPositionalArg, CliSubCommand, CliSuppress, SettingsConfigDict
 
 # Using typing_extensions vs typing:
 # https://stackoverflow.com/questions/71944041/using-modern-typing-features-on-older-versions-of-python
@@ -81,23 +77,6 @@ class DBMaintenanceSettings(BaseSettings):
         default=False,
         description="upgrade the database if it is out of date",
     )
-    version: bool = Field(default=False, description="show the current version")
-
-    add_person: bool = False
-    add_random_person: bool = False
-    delete_person: bool = False
-    edit_person: bool = False
-    list_people: bool = Field(
-        default=False,
-        description="lists the people in the database",
-    )
-    select_person: Optional[str] = None
-    view_person: bool = False
-
-    add_bank_account: Optional[str] = None
-    add_debit_card: Optional[str] = None
-    add_job: Optional[int] = None
-    provider_id: Optional[int] = None
 
     backup_path: pathlib.Path = Field(
         default=DEFAULT_BACKUP_PATH, description="directory for file backups"
@@ -124,133 +103,244 @@ class DBMaintenanceSettings(BaseSettings):
 
 
 def main() -> None:
-    try:
-        settings = DBMaintenanceSettings()  # type: ignore
-    except ValidationError as e:
-        print("error: incorrect settings - check toml, env and command options")
-        for error in e.errors():
-            print(f"{error.get('msg')}: {error.get('loc')}")
-        sys.exit(1)
+    class BankAccount(BaseSettings):
+        name: str
 
-    if settings.version:
-        print(get_version(__package__))
-        return
+    class BankAccountAdd(BankAccount):
+        attach_to_person: bool = False
 
-    if settings.db_file_path:
-        if settings.db_file_path.is_file():
-            if settings.backup_path.is_dir():
-                backup_file_path = pathlib.Path(
-                    settings.backup_path,
-                    settings.db_file_path.with_suffix(
-                        f".{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
-                        f"{settings.db_file_path.suffix}"
-                    ).name,
-                )
-                shutil.copy2(
-                    settings.db_file_path,
-                    backup_file_path,
+        def cli_cmd(self) -> None:
+            if self.attach_to_person:
+                # working_person.getBankAccountByName(self.add_bank_account)
+                pass
+            else:
+                breakpoint()
+                pass
+
+    class DebitCard(BaseSettings):
+        name: str
+
+    class DebitCardAdd(DebitCard):
+        pass
+
+        def cli_cmd(self) -> None:
+            # working_person.getDebitCardByName(self.add_debit_card)
+            pass
+
+    class Job(BaseSettings):
+        pass
+
+    class JobAdd(Job):
+        pass
+
+        def cli_cmd(self) -> None:
+            # working_person.getJobByProviderId(self.add_job)
+            pass
+
+    class Person(BaseSettings):
+        email: CliPositionalArg[str]
+
+        def cli_cmd(self) -> None:
+            CliApp.run_subcommand(self)
+
+    class PersonAdd(BaseSettings):
+        random: bool = False
+
+        def cli_cmd(self) -> None:
+            if self.random:
+                add_person = db.Person()
+                logger.warning(
+                    "Overriding user %s with randomly generated user %s",
+                    self.select_person,
+                    add_person.email,
                 )
             else:
+                print(f"adding people random={self.random}")
+                add_person = db.PersonView.add()
+                if not add_person:
+                    logger.error("user add failed")
+                    sys.exit(1)
+                logger.warning(
+                    "Overriding user %s with user %s",
+                    self.select_person,
+                    add_person.email,
+                )
+            # As email is generated, argument must be set to new email
+            self.select_person = add_person.email
+
+    class PersonDelete(Person):
+
+        def cli_cmd(self) -> None:
+            pass
+
+    class PersonEdit(Person):
+
+        def cli_cmd(self) -> None:
+            person = db.Person.findByEmail(self.email)
+            if person:
+                db.PersonView(person).edit()
+            else:
+                print("error: email not found")
+
+    class PersonView(Person):
+
+        def cli_cmd(self) -> None:
+            person = db.Person.findByEmail(self.email)
+            if person:
+                db.PersonView(person).viewDetails()
+            else:
+                print("error: email not found")
+
+    class People(BaseSettings):
+        pass
+
+    class PeopleAdd(People):
+        random: bool = False
+
+        def cli_cmd(self) -> None:
+            if self.random:
+                add_person = db.Person()
+                logger.warning(
+                    "Overriding user %s with randomly generated user %s",
+                    self.select_person,
+                    add_person.email,
+                )
+            else:
+                print(f"adding people random={self.random}")
+                add_person = db.PersonView.add()
+                if not add_person:
+                    logger.error("user add failed")
+                    sys.exit(1)
+                logger.warning(
+                    "Overriding user %s with user %s",
+                    self.select_person,
+                    add_person.email,
+                )
+            # As email is generated, argument must be set to new email
+            self.select_person = add_person.email
+
+    class PeopleList(People):
+        def cli_cmd(self) -> None:
+            print("listing persons")
+            db.PersonView.list()
+        
+    class Add(BaseSettings):
+        bank_account: CliSubCommand[BankAccountAdd]
+        debit_card: CliSubCommand[DebitCardAdd]
+        job: CliSubCommand[JobAdd]
+        people: CliSubCommand[PeopleAdd]
+
+        def cli_cmd(self) -> None:
+            CliApp.run_subcommand(self)
+
+    class Edit(BaseSettings):
+        person: CliSubCommand[PersonEdit]
+
+        def cli_cmd(self) -> None:
+            CliApp.run_subcommand(self)
+
+
+    class List(BaseSettings):
+        person: CliSubCommand[PeopleList]
+
+        def cli_cmd(self) -> None:
+            CliApp.run_subcommand(self)
+
+    class Version(BaseSettings):
+
+        def cli_cmd(self) -> None:
+            print("0.1.0")
+
+    class View(BaseSettings):
+        person: CliSubCommand[PersonView]
+
+        def cli_cmd(self) -> None:
+            CliApp.run_subcommand(self)
+
+    class Command(DBMaintenanceSettings):
+        add: CliSubCommand[Add]
+        edit: CliSubCommand[Edit]
+        list: CliSubCommand[List]
+        view: CliSubCommand[View]
+        version: CliSubCommand[Version]
+
+        def cli_cmd(self) -> None:
+            if self.db_file_path:
+                if self.db_file_path.is_file():
+                    if self.backup_path.is_dir():
+                        backup_file_path = pathlib.Path(
+                            self.backup_path,
+                            self.db_file_path.with_suffix(
+                                f".{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
+                                f"{self.db_file_path.suffix}"
+                            ).name,
+                        )
+                        shutil.copy2(
+                            self.db_file_path,
+                            backup_file_path,
+                        )
+                    else:
+                        print(
+                            f"error: incorrect backup_path {self.backup_path}, "
+                            "must be existing directory"
+                        )
+                        sys.exit(1)
+                else:
+                    if self.db_file_path != db.IN_MEMORY_DB_FILE and not self.create:
+                        print(
+                            f"error: DB file {self.db_file_path} does not exist: "
+                            "check db_file_path in toml, env and command options or use --create"
+                        )
+                        sys.exit(1)
+            else:
                 print(
-                    f"error: incorrect backup_path {settings.backup_path}, "
-                    "must be existing directory"
+                    "error: DB file path not set: check db_file_path in toml, "
+                    "env and command options"
                 )
                 sys.exit(1)
-        else:
-            if settings.db_file_path != db.IN_MEMORY_DB_FILE and not settings.create:
+
+            if not self.log_path.is_dir():
                 print(
-                    f"error: DB file {settings.db_file_path} does not exist: "
-                    "check db_file_path in toml, env and command options or use --create"
+                    f"error: incorrect log_path {self.log_path}, must be existing directory"
                 )
                 sys.exit(1)
-    else:
-        print(
-            "error: DB file path not set: check db_file_path in toml, "
-            "env and command options"
-        )
-        sys.exit(1)
 
-    if not settings.log_path.is_dir():
-        print(
-            f"error: incorrect log_path {settings.log_path}, must be existing directory"
-        )
-        sys.exit(1)
+            root_logger = logging.getLogger("")
+            root_logger.setLevel(logging.DEBUG)
 
-    root_logger = logging.getLogger("")
-    root_logger.setLevel(logging.DEBUG)
+            logging_file_handler = logging.FileHandler(
+                pathlib.Path(self.log_path, "test_db.log"),
+                mode="w",
+                encoding="utf-8",
+            )
+            logging_file_handler.setLevel(self.log_level_file)
+            logging_file_handler.setFormatter(
+                logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+            )
+            root_logger.addHandler(logging_file_handler)
 
-    logging_file_handler = logging.FileHandler(
-        pathlib.Path(settings.log_path, "test_db.log"),
-        mode="w",
-        encoding="utf-8",
-    )
-    logging_file_handler.setLevel(settings.log_level_file)
-    logging_file_handler.setFormatter(
-        logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-    )
-    root_logger.addHandler(logging_file_handler)
+            logging_stream_handler = logging.StreamHandler(sys.stdout)
+            logging_stream_handler.setLevel(self.log_level_screen)
+            logging_stream_handler.setFormatter(
+                logging.Formatter("%(levelname)s - %(message)s")
+            )
+            root_logger.addHandler(logging_stream_handler)
+            logger.debug("self=%s", self)
 
-    logging_stream_handler = logging.StreamHandler(sys.stdout)
-    logging_stream_handler.setLevel(settings.log_level_screen)
-    logging_stream_handler.setFormatter(
-        logging.Formatter("%(levelname)s - %(message)s")
-    )
-    root_logger.addHandler(logging_stream_handler)
-    logger.debug("settings=%s", settings)
+            db.databaseEncryptionKey = self.database_encryption_key.get_secret_value()
+            db.fernetIterations = self.database_fernet_iterations
 
-    db.databaseEncryptionKey = settings.database_encryption_key.get_secret_value()
-    db.fernetIterations = settings.database_fernet_iterations
+            db.DatabaseController(
+                self.db_file_path,
+                create=self.create,
+                defaultConnection=True,
+                upgrade=self.upgrade,
+            )
 
-    db.DatabaseController(
-        settings.db_file_path,
-        create=settings.create,
-        defaultConnection=True,
-        upgrade=settings.upgrade,
-    )
+            if self.add or self.edit or self.list or self.view or self.version:
+                CliApp.run_subcommand(self)
 
-    if settings.add_person:
-        add_person = db.PersonView.add()
-        if not add_person:
-            logger.error("user add failed")
-            sys.exit(1)
-        logger.warning(
-            "Overriding user %s with user %s",
-            settings.select_person,
-            add_person.email,
-        )
-        # As email is generated, argument must be set to new email
-        settings.select_person = add_person.email
-
-    if settings.add_random_person:
-        add_person = db.Person()
-        logger.warning(
-            "Overriding user %s with randomly generated user %s",
-            settings.select_person,
-            add_person.email,
-        )
-        # As email is generated, argument must be set to new email
-        settings.select_person = add_person.email
-
-    if settings.select_person:
-        working_person = db.Person.findByEmail(settings.select_person)
-        if working_person:
-            if settings.edit_person:
-                db.PersonView(working_person).edit()
-
-            if settings.add_bank_account:
-                working_person.getBankAccountByName(settings.add_bank_account)
-
-            if settings.add_debit_card:
-                working_person.getDebitCardByName(settings.add_debit_card)
-
-            if settings.add_job:
-                working_person.getJobByProviderId(settings.add_job)
-
-            if settings.view_person:
-                db.PersonView(working_person).viewDetails()
-
-    if settings.list_people:
-        db.PersonView.list()
+    CliApp.run(Command)
 
 
 if __name__ == "__main__":
