7421e2ab897113dc9d141fc9b56bdde9861d4239
Christopher Kauffman <chriskauffman@vollgas.net>
2025-10-13 11:25:01 -0500

    feat: working cli with Typer

--- src/test_db/main.py
+++ src/test_db/main.py
@@ -11,15 +11,18 @@ import pathlib
 import shutil
 import sys
 
+from typing_extensions import Annotated, Type, Tuple
+
 from pydantic import Field, SecretStr
 from pydantic_settings import (
     BaseSettings,
-    CliApp,
-    CliPositionalArg,
-    CliSubCommand,
     CliSuppress,
+    PydanticBaseSettingsSource,
     SettingsConfigDict,
+    TomlConfigSettingsSource,
 )
+
+import typer
 
 # Using typing_extensions vs typing:
 # https://stackoverflow.com/questions/71944041/using-modern-typing-features-on-older-versions-of-python
@@ -48,6 +51,7 @@ SCRIPT_NAME = "tdb"
 START_TIMESTAMP = datetime.datetime.now()
 
 logger = logging.getLogger(__name__)
+app = typer.Typer()
 
 
 def locateTomlFile() -> Optional[pathlib.Path]:
@@ -60,6 +64,176 @@ def locateTomlFile() -> Optional[pathlib.Path]:
     return None
 
 
+@app.callback()
+def app_callback(
+    db_file_path: pathlib.Path,
+    create: Annotated[
+        bool,
+        typer.Option(
+            help="databases are not created by default, creates the database file when True"
+        ),
+    ] = False,
+    upgrade: Annotated[
+        bool, typer.Option(help="upgrade the database if it is out of date")
+    ] = False,
+):
+    print(db_file_path)
+    settings = DBMaintenanceSettings()
+    if db_file_path:
+        if db_file_path.is_file():
+            if settings.backup_path.is_dir():
+                backup_file_path = pathlib.Path(
+                    settings.backup_path,
+                    db_file_path.with_suffix(
+                        f".{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
+                        f"{db_file_path.suffix}"
+                    ).name,
+                )
+                shutil.copy2(
+                    db_file_path,
+                    backup_file_path,
+                )
+            else:
+                print(
+                    f"error: incorrect backup_path {settings.backup_path}, "
+                    "must be existing directory"
+                )
+                sys.exit(1)
+        else:
+            if db_file_path != db.IN_MEMORY_DB_FILE and not create:
+                print(
+                    f"error: DB file {db_file_path} does not exist: "
+                    "check db_file_path in toml, env and command options or use --create"
+                )
+                sys.exit(1)
+    else:
+        print(
+            "error: DB file path not set: check db_file_path in toml, "
+            "env and command options"
+        )
+        sys.exit(1)
+
+    if not settings.log_path.is_dir():
+        print(
+            f"error: incorrect log_path {settings.log_path}, must be existing directory"
+        )
+        sys.exit(1)
+
+    root_logger = logging.getLogger("")
+    root_logger.setLevel(logging.DEBUG)
+
+    logging_file_handler = logging.FileHandler(
+        pathlib.Path(settings.log_path, "test_db.log"),
+        mode="w",
+        encoding="utf-8",
+    )
+    logging_file_handler.setLevel(settings.log_level_file)
+    logging_file_handler.setFormatter(
+        logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+    )
+    root_logger.addHandler(logging_file_handler)
+
+    logging_stream_handler = logging.StreamHandler(sys.stdout)
+    logging_stream_handler.setLevel(settings.log_level_screen)
+    logging_stream_handler.setFormatter(
+        logging.Formatter("%(levelname)s - %(message)s")
+    )
+    root_logger.addHandler(logging_stream_handler)
+    logger.debug("settings=%s", settings)
+
+    db.databaseEncryptionKey = settings.database_encryption_key.get_secret_value()
+    db.fernetIterations = settings.database_fernet_iterations
+
+    db.DatabaseController(
+        db_file_path,
+        create=create,
+        defaultConnection=True,
+        upgrade=upgrade,
+    )
+
+
+@app.command()
+def version():
+    print(get_version(__package__))
+
+
+add_app = typer.Typer()
+app.add_typer(add_app, name="add")
+
+
+@add_app.command("bank-account")
+def bank_account_add(
+    description: Optional[str] = None, person_email: Optional[str] = None
+):
+    if person_email:
+        person = db.Person.findByEmail(person_email)
+        if person:
+            person.getBankAccountByName(description or "default")
+        else:
+            print("error: email not found")
+    else:
+        db.BankAccount(description=description)
+
+
+@add_app.command("debit-card")
+def debit_card_add(
+    description: Optional[str] = None, person_email: Optional[str] = None
+):
+    if person_email:
+        person = db.Person.findByEmail(person_email)
+        if person:
+            person.getDebitCardByName(description or "default")
+        else:
+            print("error: email not found")
+    else:
+        db.DebitCard(description=description)
+
+
+@add_app.command("person")
+def person_add(random: bool = False):
+    if random:
+        print(f"{db.Person().email} added")
+    else:
+        new_person = db.PersonView.add()
+        if new_person:
+            print(f"{new_person.email} added")
+
+
+edit_app = typer.Typer()
+app.add_typer(edit_app, name="edit")
+
+
+@edit_app.command("person")
+def person_edit(email: str):
+    person = db.Person.findByEmail(email)
+    if person:
+        db.PersonView(person).edit()
+    else:
+        print("error: email not found")
+
+
+list_app = typer.Typer()
+app.add_typer(list_app, name="list")
+
+
+@list_app.command("people")
+def people_list():
+    db.PersonView.list()
+
+
+view_app = typer.Typer()
+app.add_typer(view_app, name="view")
+
+
+@view_app.command("person")
+def person_view(email: str, db_file_path: Optional[pathlib.Path] = None):
+    person = db.Person.findByEmail(email)
+    if person:
+        db.PersonView(person).viewDetails()
+    else:
+        print("error: email not found")
+
+
 class DBMaintenanceSettings(BaseSettings):
     """Test DB DB Maintenance Script
 
@@ -67,20 +241,9 @@ class DBMaintenanceSettings(BaseSettings):
     """
 
     model_config = SettingsConfigDict(
-        cli_implicit_flags=True,
-        cli_parse_args=True,
         env_file=".env",
         extra="ignore",
         toml_file=locateTomlFile(),
-    )
-
-    create: bool = Field(
-        default=False,
-        description="databases are not created by default, creates the database file when True",
-    )
-    upgrade: bool = Field(
-        default=False,
-        description="upgrade the database if it is out of date",
     )
 
     backup_path: pathlib.Path = Field(
@@ -91,9 +254,11 @@ class DBMaintenanceSettings(BaseSettings):
         description="key to be used for encrypting sensitive database contents",
     )
     database_fernet_iterations: int = Field(
-        1_200_000, description="number of iterations for fernet key generation"
+        default=1_200_000, description="number of iterations for fernet key generation"
     )
-    db_file_path: Optional[pathlib.Path] = Field(None, description="database file")
+    db_file_path: Optional[pathlib.Path] = Field(
+        default=None, description="database file"
+    )
     log_level_file: Union[
         int, Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
     ] = Field(default="INFO", description="log level for file storage of log messages")
@@ -106,201 +271,35 @@ class DBMaintenanceSettings(BaseSettings):
         default=DEFAULT_LOG_PATH, description="directory for logging activity"
     )
 
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        """Add TOML to sources"""
+        # pylint: disable=too-many-arguments
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            TomlConfigSettingsSource(settings_cls),
+        )
+
+        # if self.attach_to_person:
+        # working_person.getBankAccountByName(self.add_bank_account)
+
+        # working_person.getDebitCardByName(self.add_debit_card)
+
+        # working_person.getJobByProviderId(self.add_job)
+
 
 def main() -> None:
-    class BankAccount(BaseSettings):
-        name: str
-
-    class BankAccountAdd(BankAccount):
-        attach_to_person: bool = False
-
-        def cli_cmd(self) -> None:
-            if self.attach_to_person:
-                # working_person.getBankAccountByName(self.add_bank_account)
-                pass
-            else:
-                pass
-
-    class DebitCard(BaseSettings):
-        name: str
-
-    class DebitCardAdd(DebitCard):
-        pass
-
-        def cli_cmd(self) -> None:
-            # working_person.getDebitCardByName(self.add_debit_card)
-            pass
-
-    class Job(BaseSettings):
-        pass
-
-    class JobAdd(Job):
-        pass
-
-        def cli_cmd(self) -> None:
-            # working_person.getJobByProviderId(self.add_job)
-            pass
-
-    class Person(BaseSettings):
-        email: CliPositionalArg[str]
-
-        def cli_cmd(self) -> None:
-            CliApp.run_subcommand(self)
-
-    class PersonDelete(Person):
-        def cli_cmd(self) -> None:
-            pass
-
-    class PersonEdit(Person):
-        def cli_cmd(self) -> None:
-            person = db.Person.findByEmail(self.email)
-            if person:
-                db.PersonView(person).edit()
-            else:
-                print("error: email not found")
-
-    class PersonView(Person):
-        def cli_cmd(self) -> None:
-            person = db.Person.findByEmail(self.email)
-            if person:
-                db.PersonView(person).viewDetails()
-            else:
-                print("error: email not found")
-
-    class People(BaseSettings):
-        pass
-
-    class PeopleAdd(People):
-        random: bool = False
-
-        def cli_cmd(self) -> None:
-            if self.random:
-                print(f"{db.Person().email} added")
-            else:
-                new_person = db.PersonView.add()
-                if new_person:
-                    print(f"{new_person.email} added")
-
-    class PeopleList(People):
-        def cli_cmd(self) -> None:
-            print("listing persons")
-            db.PersonView.list()
-
-    class Add(BaseSettings):
-        bank_account: CliSubCommand[BankAccountAdd]
-        debit_card: CliSubCommand[DebitCardAdd]
-        job: CliSubCommand[JobAdd]
-        people: CliSubCommand[PeopleAdd]
-
-        def cli_cmd(self) -> None:
-            CliApp.run_subcommand(self)
-
-    class Edit(BaseSettings):
-        person: CliSubCommand[PersonEdit]
-
-        def cli_cmd(self) -> None:
-            CliApp.run_subcommand(self)
-
-    class List(BaseSettings):
-        person: CliSubCommand[PeopleList]
-
-        def cli_cmd(self) -> None:
-            CliApp.run_subcommand(self)
-
-    class Version(BaseSettings):
-        def cli_cmd(self) -> None:
-            print(get_version(__package__))
-
-    class View(BaseSettings):
-        person: CliSubCommand[PersonView]
-
-        def cli_cmd(self) -> None:
-            CliApp.run_subcommand(self)
-
-    class Command(DBMaintenanceSettings):
-        add: CliSubCommand[Add]
-        edit: CliSubCommand[Edit]
-        list: CliSubCommand[List]
-        view: CliSubCommand[View]
-        version: CliSubCommand[Version]
-
-        def cli_cmd(self) -> None:
-            if self.db_file_path:
-                if self.db_file_path.is_file():
-                    if self.backup_path.is_dir():
-                        backup_file_path = pathlib.Path(
-                            self.backup_path,
-                            self.db_file_path.with_suffix(
-                                f".{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
-                                f"{self.db_file_path.suffix}"
-                            ).name,
-                        )
-                        shutil.copy2(
-                            self.db_file_path,
-                            backup_file_path,
-                        )
-                    else:
-                        print(
-                            f"error: incorrect backup_path {self.backup_path}, "
-                            "must be existing directory"
-                        )
-                        sys.exit(1)
-                else:
-                    if self.db_file_path != db.IN_MEMORY_DB_FILE and not self.create:
-                        print(
-                            f"error: DB file {self.db_file_path} does not exist: "
-                            "check db_file_path in toml, env and command options or use --create"
-                        )
-                        sys.exit(1)
-            else:
-                print(
-                    "error: DB file path not set: check db_file_path in toml, "
-                    "env and command options"
-                )
-                sys.exit(1)
-
-            if not self.log_path.is_dir():
-                print(
-                    f"error: incorrect log_path {self.log_path}, must be existing directory"
-                )
-                sys.exit(1)
-
-            root_logger = logging.getLogger("")
-            root_logger.setLevel(logging.DEBUG)
-
-            logging_file_handler = logging.FileHandler(
-                pathlib.Path(self.log_path, "test_db.log"),
-                mode="w",
-                encoding="utf-8",
-            )
-            logging_file_handler.setLevel(self.log_level_file)
-            logging_file_handler.setFormatter(
-                logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-            )
-            root_logger.addHandler(logging_file_handler)
-
-            logging_stream_handler = logging.StreamHandler(sys.stdout)
-            logging_stream_handler.setLevel(self.log_level_screen)
-            logging_stream_handler.setFormatter(
-                logging.Formatter("%(levelname)s - %(message)s")
-            )
-            root_logger.addHandler(logging_stream_handler)
-            logger.debug("self=%s", self)
-
-            db.databaseEncryptionKey = self.database_encryption_key.get_secret_value()
-            db.fernetIterations = self.database_fernet_iterations
-
-            db.DatabaseController(
-                self.db_file_path,
-                create=self.create,
-                defaultConnection=True,
-                upgrade=self.upgrade,
-            )
-
-            if self.add or self.edit or self.list or self.view or self.version:
-                CliApp.run_subcommand(self)
-
-    CliApp.run(Command)
+    app()
 
 
 if __name__ == "__main__":
