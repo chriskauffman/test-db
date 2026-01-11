b981b16d59e9268f1fc15f65f18a039f67dacfa5
Christopher Kauffman <chriskauffman@vollgas.net>
2026-01-11 09:25:33 -0600

    test: moved tdb and tdb_console conftest items to cli/conftest.py

---
+++ tests/cli/conftest.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+import test_db
+
+
+@pytest.fixture(scope="module")
+def person(temporary_db):
+    return test_db.Person(connection=temporary_db.connection)
+
+
+@pytest.fixture(scope="module")
+def organization(temporary_db):
+    return test_db.Organization(connection=temporary_db.connection)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env():
+    os.environ["DATABASE_ENCRYPTION_KEY"] = "a test encryption key"
+    os.environ["LOG_PATH"] = "log"
