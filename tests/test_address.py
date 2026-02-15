48554c4f99760970c04487328941f03902198d0e
Christopher Kauffman <chriskauffman@vollgas.net>
2026-02-15 09:20:52 -0600

    test: add new tests to check automation creation

--- tests/test_address.py
+++ tests/test_address.py
@@ -1,3 +1,4 @@
+import test_db
 from test_db._organization import Organization
 from test_db._organization_address import OrganizationAddress
 from test_db._person import Person
@@ -18,6 +19,51 @@ def test_organization_address(temporary_db):
     assert address_validation(test_address)
 
 
+def test_autoCreateDependents_organization(temporary_db):
+    new_organization = Organization(connection=temporary_db.connection)
+    organization_count = Organization.select(connection=temporary_db.connection).count()
+
+    test_db.autoCreateDependents = False
+    test_address = OrganizationAddress(
+        connection=temporary_db.connection,
+    )
+    assert test_address.organization is None
+    assert (
+        Organization.select(connection=temporary_db.connection).count()
+        == organization_count
+    )
+
+    test_address = OrganizationAddress(
+        organization=new_organization,
+        connection=temporary_db.connection,
+    )
+    assert test_address.organization is new_organization
+    assert (
+        Organization.select(connection=temporary_db.connection).count()
+        == organization_count
+    )
+
+    test_db.autoCreateDependents = True
+    test_address = OrganizationAddress(
+        connection=temporary_db.connection,
+    )
+    assert isinstance(test_address.organization, Organization)
+    assert (
+        Organization.select(connection=temporary_db.connection).count()
+        == organization_count + 1
+    )
+
+    test_address = OrganizationAddress(
+        organization=new_organization,
+        connection=temporary_db.connection,
+    )
+    assert test_address.organization is new_organization
+    assert (
+        Organization.select(connection=temporary_db.connection).count()
+        == organization_count + 1
+    )
+
+
 def test_person_address(temporary_db):
     test_address = PersonAddress(
         person=Person(connection=temporary_db.connection),
@@ -26,3 +72,36 @@ def test_person_address(temporary_db):
 
     assert isinstance(test_address, PersonAddress)
     assert address_validation(test_address)
+
+
+def test_autoCreateDependents_person(temporary_db):
+    new_person = Person(connection=temporary_db.connection)
+    person_count = Person.select(connection=temporary_db.connection).count()
+
+    test_db.autoCreateDependents = False
+    test_address = PersonAddress(
+        connection=temporary_db.connection,
+    )
+    assert test_address.person is None
+    assert Person.select(connection=temporary_db.connection).count() == person_count
+
+    test_address = PersonAddress(
+        person=new_person,
+        connection=temporary_db.connection,
+    )
+    assert test_address.person is new_person
+    assert Person.select(connection=temporary_db.connection).count() == person_count
+
+    test_db.autoCreateDependents = True
+    test_address = PersonAddress(
+        connection=temporary_db.connection,
+    )
+    assert isinstance(test_address.person, Person)
+    assert Person.select(connection=temporary_db.connection).count() == person_count + 1
+
+    test_address = PersonAddress(
+        person=new_person,
+        connection=temporary_db.connection,
+    )
+    assert test_address.person is new_person
+    assert Person.select(connection=temporary_db.connection).count() == person_count + 1
